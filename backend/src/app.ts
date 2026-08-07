10608378f29d281808ed891303147a6290cd7f77
Spilitious <lordThergal@gmail.com>
2026-08-07 22:11:41 +0200

    Add health API endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes/index.js";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", routes);
+
 app.get("/", (req, res) => {
-  res.send("Langlift backend OK");
+  res.send("Langlift backend OK !");
 });
 
 export default app;
